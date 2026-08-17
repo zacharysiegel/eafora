2a5269815afb4826fd0b36d67a1e561abb69d2f8
zacharysiegel <siegelzc@gmail.com>
2026-08-17 13:17:51 -0500

    hash asset filenames so the deploy can serve /pkg/* as immutable

--- web/src/app.rs
+++ web/src/app.rs
@@ -1,6 +1,7 @@
 use leptos::hydration::{AutoReload, HydrationScripts};
 use leptos::prelude::*;
 use leptos_i18n::context::{CookieOptions, UseLocalesOptions};
+use leptos_meta::HashedStylesheet;
 use leptos_router::components::{Route, Router, Routes};
 use leptos_router::StaticSegment;
 
@@ -10,11 +11,11 @@ use crate::map::MapView;
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
         <!DOCTYPE html>
-        <html lang="en">
+        <html>
             <head>
                 <meta charset="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="stylesheet" id="leptos" href="/pkg/eafora.css" />
+                <HashedStylesheet options=options.clone() id="leptos" />
                 <AutoReload options=options.clone() />
                 <HydrationScripts options />
             </head>
